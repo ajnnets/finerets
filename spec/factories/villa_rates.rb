eee0f8a28b571b75f032ca5e5c08f50038d3f6eb
Prelang Builder <builder@prelang.com>
2016-06-29 21:40:31 +0000

    Generated model VillaRate with migration

---
+++ spec/factories/villa_rates.rb
@@ -0,0 +1,11 @@
+FactoryGirl.define do
+  factory :villa_rate do
+    from "2016-06-29 21:40:31"
+    to "2016-06-29 21:40:31"
+    season "MyString"
+    minimum_stay "MyString"
+    rate "MyString"
+    villa nil
+    country nil
+  end
+end
