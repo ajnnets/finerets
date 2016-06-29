adc565e17f50a520a6242992a503d62e30489a45
Prelang Builder <builder@prelang.com>
2016-06-29 21:42:07 +0000

    Permitted paramaters (attributes) in ActiveAdmin configuration for 'NeedToKnow' to allow for create, update

--- app/admin/need_to_know.rb
+++ app/admin/need_to_know.rb
@@ -1,4 +1,5 @@
 ActiveAdmin.register NeedToKnow do
+  permit_params :points
 
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
