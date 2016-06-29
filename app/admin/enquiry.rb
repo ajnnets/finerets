42ba21b717a2371afc04c974e762c76e337aee7d
Prelang Builder <builder@prelang.com>
2016-06-29 21:42:38 +0000

    Permitted paramaters (attributes) in ActiveAdmin configuration for 'Enquiry' to allow for create, update

--- app/admin/enquiry.rb
+++ app/admin/enquiry.rb
@@ -1,4 +1,5 @@
 ActiveAdmin.register Enquiry do
+  permit_params :first_name, :last_name, :email, :description, :read
 
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
