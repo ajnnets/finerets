6754e039a10ba3d9e33a0812fb086179d3aaecac
Prelang Builder <builder@prelang.com>
2016-06-29 21:42:32 +0000

    Permitted paramaters (attributes) in ActiveAdmin configuration for 'Villa' to allow for create, update

--- app/admin/villa.rb
+++ app/admin/villa.rb
@@ -1,4 +1,5 @@
 ActiveAdmin.register Villa do
+  permit_params :name, :description, :guest_count, :bedroom_count, :villa_detail_heading, :love_points_heading, :villa_rates_heading, :need_to_know_heading, :feature_and_facility_heading, :similar_villas, :published
 
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
